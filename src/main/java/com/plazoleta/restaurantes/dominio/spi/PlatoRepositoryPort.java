6fd1634ac7e0e03ba92fce9142b6e6a5ce12056f
AlssyLop <alcibiades.lopezf@gmail.com>
2026-06-05 13:05:11 -0500

    Revert "refactor(persistencia): agregar busqueda de plato por id en repositorio"
    This reverts commit c11fff8ccbd6e6f45e8757d4707f694ef4d6e36e.

--- src/main/java/com/plazoleta/restaurantes/dominio/spi/PlatoRepositoryPort.java
+++ src/main/java/com/plazoleta/restaurantes/dominio/spi/PlatoRepositoryPort.java
@@ -3,10 +3,7 @@ package com.plazoleta.restaurantes.dominio.spi;
 import com.plazoleta.restaurantes.dominio.modelo.Plato;
 import com.plazoleta.restaurantes.dominio.modelo.value.NombrePlato;
 
-import java.util.Optional;
-
 public interface PlatoRepositoryPort {
     Plato save(Plato plato);
     boolean existsByNombreAndIdRestaurante(NombrePlato nombre, Long idRestaurante);
-    Optional<Plato> findById(Long id);
 }
